243b2e34a80b28a28929b8e99669aa8ba65ad3bb
profesorx <jddiaz@misena.edu.co>
2026-02-22 13:59:29 -0500

    first commit

---
+++ src/main/java/edu/unir/app/GestionEstudiantesGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.unir.app;
+
+/**
+ *
+ * @author SENA
+ */
+public class GestionEstudiantesGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
